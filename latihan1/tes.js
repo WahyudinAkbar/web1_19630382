c06811cd0005105f8104c258d9bea1cf2a9a493b
WahyudinAkbar <wahyugamers07@gmail.com>
2020-12-03 12:31:04 +0800

    kumpul web1 - Muhammad Wahyudin Akbar

---
+++ latihan1/tes.js
@@ -0,0 +1,9 @@
+for (var i=0;i<10;i++) {
+  if (i%2==0) {
+    console.log(`${i} Angka Genap`);
+  } else if (i%3==0) {
+    console.log(`${i} Angka Kelipatan 3`);
+  } else {
+    console.log(`${i} Angka Ganjil`);
+  }
+}
